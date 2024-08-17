fc8114983c6faeb5f03bf6fc9f721b1cf7925e71
Maja Bukowska <majabukowska8@gmail.com>
2024-08-17 21:12:21 +0200

    serializers fix

--- shelters_api/serializers.py
+++ shelters_api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Pet, Shelter, User
 
 class PetSerializer(serializers.ModelSerializer):
-    image_url = serializers.ImageField(use_url=True, required=False, allow_null=True)
 
     class Meta:
         model = Pet
@@ -10,7 +9,6 @@ class PetSerializer(serializers.ModelSerializer):
 
 class ShelterSerializer(serializers.ModelSerializer):
     pets = PetSerializer(many=True, read_only=True)
-    image_url = serializers.ImageField(use_url=True, required=False, allow_null=True)  # Add this if Shelter has an ImageField
 
     class Meta:
         model = Shelter
